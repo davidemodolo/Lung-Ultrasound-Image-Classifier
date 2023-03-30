eceacc9cd33b52fa41159d0b64d7036267bbce6f
Davide Modolo <davide.modolo98@gmail.com>
2023-03-30 16:32:03 +0200

    final model trained

--- dataset_utilities/mean_sd_dataset.py
+++ dataset_utilities/mean_sd_dataset.py
@@ -10,21 +10,36 @@ from tqdm import tqdm
 IMAGES_PATH = "images"
 
 # initialize the list of means and standard deviations
-means = np.zeros(3)
-stds = np.zeros(3)
+means_r = []
+means_g = []
+means_b = []
+stds_r = []
+stds_g = []
+stds_b = []
 counter = 0
 
 # loop over the image paths
 for imagePath in tqdm(os.listdir(IMAGES_PATH)):
     # compute the mean and standard deviation of each channel
     image = cv2.imread(os.path.join(IMAGES_PATH, imagePath))
-    means += np.mean(image, axis=(0, 1))
-    stds += np.std(image, axis=(0, 1))
+    means_b.append(np.mean(image[:, :, 0]))
+    means_g.append(np.mean(image[:, :, 1]))
+    means_r.append(np.mean(image[:, :, 2]))
+    stds_b.append(np.std(image[:, :, 0]))
+    stds_g.append(np.std(image[:, :, 1]))
+    stds_r.append(np.std(image[:, :, 2]))
     counter += 1
 
 # compute the mean and standard deviation of the images
-means /= counter
-stds /= counter
+means = np.zeros(3)	
+stds = np.zeros(3)
+
+means[0] = np.mean(means_r)
+means[1] = np.mean(means_g)
+means[2] = np.mean(means_b)
+stds[0] = np.mean(stds_r)
+stds[1] = np.mean(stds_g)
+stds[2] = np.mean(stds_b)
 
 # show the computed statistics in the format of transform.Normalize([x, x, x], [x, x, x])
 print("mean = {}".format(means))
